614da825f51522276c36f726203ec7b103e816cd
JASE084 <jasonbakker084@gmail.com>
2019-08-05 11:47:57 +0200

    Add message te Sweet

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,57 @@
-(function () {
-   "use strict";
-}());
+"use strict";
 
+
+// basic flavours 
+
+
+function message() {
+    var typeOfFlavour = document.getElementById("orangeNectar");
+    typeOfFlavour.addEventListener("click", message);
+    alert(flavours.sweet.name + " " + flavours.sweet.color);
+}
+
+message();
+
+
+var sweet = document.getElementById("orangeNectar");
+var floral = document.getElementById("gipsyPink");
+var sourFermented = document.getElementById("yolk");
+var greenVegetative = document.getElementById("amazonMoss");
+var other = document.getElementById("dreamICanFly");
+var roasted = document.getElementById("tomatoTango");
+var spices = document.getElementById("candyCaneRed");
+var nuttyCocoa = document.getElementById("amaretto");
+var fruity = document.getElementById("redstone");
+
+//// subFlavours
+//
+//
+//var blackTea = document.getElementById(".inner-1-blackTea");
+//var subFlavourFloral = document.getElementById(".inner-1-floral");
+//var berry = document.getElementById(".inngetElementByIder-1-berry");
+//var driedFruit = document.getElementById(".inner-1-driedFruit");
+//var otherFruit = document.getElementById(".inner-1-otherFruit");
+//var citrusFruit = document.getElementById(".inner-1-citrusFruit");
+//var sour = document.getElementById(".inner-1-sour");
+//var alcoholFermented = document.getElementById(".inner-1-alcoholFermented");
+//var oliveOil = document.getElementById(".inner-1-oliveOil");
+//var raw = document.getElementById(".inner-1-raw");
+//var subFlavourGreenVegetative = document.getElementById(".inner-1-greenVegetative");
+//var beany = document.getElementById(".inner-1-beany");
+//var paperMusty = document.getElementById(".inner-1-paperMusty");
+//var chemical = document.getElementById(".inner-1-chemical");
+//var pipeTobacco = document.getElementById(".inner-1-pipeTobacco");
+//var tobacco = document.getElementById(".inner-1-tobacco");
+//var burnt = document.getElementById(".inner-1-burnt");
+//var cereal = document.getElementById(".inner-1-cereal");
+//var pungent = document.getElementById(".inner-1-pungent");
+//var pepper = document.getElementById(".inner-1-pepper");
+//var brownSpice = document.getElementById(".inner-1-brownSpice");
+//var nutty = document.getElementById(".inner-1-nutty");
+//var cocoa = document.getElementById(".inner-1-cocoa");
+//var brownSugar = document.getElementById(".inner-1-brownSugar");
+//var vanilla = document.getElementById(".inner-1-vanilla");
+//var vanillin = document.getElementById(".inner-1-vanillin");
+//var overallSweet = document.getElementById(".inner-1-overallSweet");
+//var sweetAromatics = document.getElementById(".inner-1-sweetAromatics");
+//var subFlavourSourFermented = document.getElementById(".inner-1-sourFermented");
