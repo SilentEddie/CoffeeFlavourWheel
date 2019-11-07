285ac74074ad46b4ad63432b0d6d44920b4993c6
Edward Idema <edwardidema@gmail.com>
2019-11-07 14:21:56 +0100

    initial commit

---
+++ src/main/java/com/novi/edward/tastecoffee/User.java
@@ -0,0 +1,17 @@
+package com.novi.edward.tastecoffee;
+import java.util.ArrayList;
+/**
+ * @author edward idema edwardidema@gmail.com
+ */
+public class User {
+    protected String name;
+    protected String emailadres;
+    private String password;
+    protected ArrayList<TastedCoffee> tastedCoffees;
+    
+    
+    
+            
+    
+    
+}
