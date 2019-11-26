ee724bc4b4f028af9adb2294b7cb3d3d19d95a9f
Jason Bakker <jasonbakker084@gmail.com>
2019-11-26 11:32:44 +0100

    no comment

--- src/main/java/com/teamcoffee/coffeeflavourwheel/repository/DBFileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.teamcoffee.coffeeflavourwheel.repository;
-
-import com.teamcoffee.coffeeflavourwheel.model.DBFile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DBFileRepository extends JpaRepository<DBFile, String> {
-}
--- src/main/java/com/teamcoffee/coffeeflavourwheel/repository/MessageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.teamcoffee.coffeeflavourwheel.repository;
-
-import com.teamcoffee.coffeeflavourwheel.model.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<Message, Integer> {
-
-}
